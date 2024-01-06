1d5f40c19646166ec51d992639108d2fbc74c852
RojuGhimire <ghimireroju@gmail.com>
2024-01-06 23:00:40 +0545

    cursor

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 
 import './App.css'
 import PostForm from './PostForm'
-
+import Cursor from './Cursor'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Cursor />
    <PostForm/ >
 
     </>
